df612576fe8709ef314f9774141d1596074d5055
Walid Khan <walidkhan02013@gmail.com>
2025-01-05 20:35:45 +0600

    CWH Class-1 JS done

---
+++ JS/cwhClass1.js
@@ -0,0 +1,18 @@
+
+var a = prompt("Enter your favorite number : ");
+console.log("Code is running");
+
+console.log("Your number is "+ a);
+var isTrue = confirm("Are your sure you want to leave this page && blast your computer");
+
+if(isTrue)
+{
+    console.log("Computer is blasting");
+
+}
+else{
+    console.log("Your computer survived")
+}
+
+document.title="This is code with harry!"
+document.body.style.backgroundColor= "blue"
